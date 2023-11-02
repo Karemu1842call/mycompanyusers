c2693af7f518f57695048961c68d4e0352056db4
karemu1842call <victorkinoti03@gmail.com>
2023-11-02 10:01:09 +0300

    configured

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Page() {
   const [data, setData] = useState([]);
   const [selectedData, setSelectedData] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleShowMore = (data) => {
     setSelectedData(data);
@@ -18,30 +18,29 @@ export default function Page() {
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("https://176.58.110.189/api/member/");
         const result = await response.json();
         setData(result);
+        console.log(data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, []); 
+  }, []);
 
-
-   if (!isLoggedIn) {
+  if (!isLoggedIn) {
     return <Loginform setIsLoggedIn={setIsLoggedIn} />;
   }
 
   if (!data || data.length === 0) {
     return <Loading />;
   }
-  
 
   return (
     <div className="box-container ">
@@ -55,20 +54,20 @@ export default function Page() {
       <div className="card-container">
         {data.map((item) => (
           <div class="card hover:scale-105 " key={item.id}>
-            <div className="hover:scale-105 text-gray-800 ">
-              <h1 className="font-bold text-center text-2xl mb-3 underline">User Details</h1>
-              <h2>
-                Name: {item.user.first_name} {item.user.last_name}
-              </h2>
-              <p>Personal ID: {item.personal_id}</p>
-              <p>Phone Number: {item.phone_no}</p>
-              <p>Gender: {item.gender}</p>
-              <p>Marital Status: {item.marital_status}</p>
-              <p>Date of Birth: {item.dob}</p>
-              <p>Tax ID: {item.tax_id_no}</p>
-              <p>Postal Address: {item.postal_address}</p>
+            <div className="hover:scale-105 ">
+              <h1 className="font-bold text-center text-2xl mb-3 underline text-blue-800">
+                User Details
+              </h1>
+              <div className="text-orange-700">
+                <h2>
+                  Name: {item.user.first_name} {item.user.last_name}
+                </h2>
+                <h2>Personal ID: {item.personal_id}</h2>
+                <h2>Phone Number: {item.phone_no}</h2>
+                <h2>Gender: {item.gender}</h2>
+                <h2>Marital Status: {item.marital_status}</h2>
+              </div>
 
-              
               <button
                 onClick={() => handleShowMore(item)}
                 className="bg-green-500 px-2 rounded-lg py-2 text-gray-100 hover:scale-105 ml-10 active:scale-100"
@@ -79,7 +78,11 @@ export default function Page() {
           </div>
         ))}
       </div>
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal} data={selectedData}/>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        data={selectedData}
+      />
     </div>
   );
 }
