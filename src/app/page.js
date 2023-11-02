f9af049bbe5edddcdcde5379538425f66e768788
karemu1842call <victorkinoti03@gmail.com>
2023-11-02 10:12:08 +0300

    configured card

--- src/app/page.js
+++ src/app/page.js
@@ -53,29 +53,38 @@ export default function Page() {
       </h2>
       <div className="card-container">
         {data.map((item) => (
-          <div class="card hover:scale-105 " key={item.id}>
-            <div className="hover:scale-105 ">
-              <h1 className="font-bold text-center text-2xl mb-3 underline text-blue-800">
-                User Details
-              </h1>
-              <div className="text-orange-700">
-                <h2>
-                  Name: {item.user.first_name} {item.user.last_name}
-                </h2>
-                <h2>Personal ID: {item.personal_id}</h2>
-                <h2>Phone Number: {item.phone_no}</h2>
-                <h2>Gender: {item.gender}</h2>
-                <h2>Marital Status: {item.marital_status}</h2>
-              </div>
-
-              <button
-                onClick={() => handleShowMore(item)}
-                className="bg-green-500 px-2 rounded-lg py-2 text-gray-100 hover:scale-105 ml-10 active:scale-100"
-              >
-                Show more
-              </button>
-            </div>
+        <div className="card hover:scale-105" key={item.id}>
+        <div className="p-4 border border-gray-300 rounded-lg">
+          <h1 className="font-bold text-center text-2xl mb-3 underline text-blue-800">
+            User Details
+          </h1>
+          <div className="flex flex-col items-start mb-4">
+            <p className="text-stone-900 font-bold">
+              <span className="font-semibold">Name:</span> {item.user.first_name} {item.user.last_name}
+            </p>
+            <p className="text-stone-900 font-bold">
+              <span className="font-semibold">Personal ID:</span> {item.personal_id}
+            </p>
+            <p className="text-stone-900 font-bold">
+              <span className="font-semibold">Phone Number:</span> {item.phone_no}
+            </p>
+            <p className="text-stone-900 font-bold">
+              <span className="font-semibold">Gender:</span> {item.gender}
+            </p>
+            <p className="text-stone-900 font-bold">
+              <span className="font-semibold">Marital Status:</span> {item.marital_status}
+            </p>
           </div>
+      
+          <button
+            onClick={() => handleShowMore(item)}
+            className="bg-green-500 px-4 py-2 rounded-lg text-gray-100 hover:bg-green-600 transition duration-300"
+          >
+            Show more
+          </button>
+        </div>
+      </div>
+      
         ))}
       </div>
       <Modal
