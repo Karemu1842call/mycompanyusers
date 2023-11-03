53482a9096757cdf2279188477e0bc49f73f7a7f
karemu1842call <victorkinoti03@gmail.com>
2023-11-03 13:49:41 +0300

    key

--- src/app/Modal.js
+++ src/app/Modal.js
@@ -28,7 +28,7 @@ const Modal = ({ isOpen, onClose, data }) => {
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div
         className="modal-container bg-lime-300 p-8 rounded-lg shadow-lg"
-        key={data.user_id}
+        key={data.id}
       >
         <aside className="flex">
           <h2 className="text-2xl font-bold mb-4">User Details</h2>
@@ -108,6 +108,7 @@ const Modal = ({ isOpen, onClose, data }) => {
                     </span>
                   </h3>
                   <h3>Email: {entity.email}</h3>
+                  <img src={entity.logo} alt={`Logo for ${entity.name}`} />
                 </li>
               )}
             </div>
