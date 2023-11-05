d668cfa76864621a74bdc55f6eb8ad2c8d9174e5
karemu1842call <victorkinoti03@gmail.com>
2023-11-05 16:52:48 +0300

    ,

--- src/app/Loading.js
+++ src/app/Loading.js
@@ -3,11 +3,8 @@ import React from "react";
 export default function Loading() {
   return (
     <div className="loading-container ">
-      <div className="mb-60">
-        <h1 className="head-text text-cyan-500 text-center text-3xl font-bold">
-          Company <span className="text-white">-</span>
-          <span className="text-green-500"> Users. </span>
-        </h1>
+      <div className="">
+       
       </div>
       <div className="loader"></div>
       <p className="font-bold text-2xl"> Loading...</p>
